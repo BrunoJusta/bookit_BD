9148a97ee0779a56bb5cbe490c32b183da85b930
BrunoJusta <brunomajusta@gmail.com>
2020-05-30 13:44:10 +0100

    F's in the Chat Boyz

--- index.js
+++ index.js
@@ -26,10 +26,13 @@ app.use(bodyParser.urlencoded({
 app.use(bodyParser.json());
 
 
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
+
+app.use(function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', "*");
+    res.header('Access-Control-Allow-Methods','GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
     next();
-});
+})
 
 
 
