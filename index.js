ce05fe2efe9adc3613680cb5dbf394b3a0b9307c
BrunoJusta <brunomajusta@gmail.com>
2020-06-02 01:07:21 +0100

    sd

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ app.use(cors({origin:'*'}));
 
 app.use(function(req, res, next) {
     res.header('Access-Control-Allow-Origin', "*");
-    res.header('Access-Control-Allow-Methods','GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Methods','GET,PUT,POST,DELETE,OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Content-Type');
     next();
 })
