39f8da892f3e0e934cf72f7f8096ca04d07d7a41
Nuno9180580 <9180580@esmad.ipp.pt>
2020-06-24 16:09:43 +0100

    asd

--- controllers/workshops/inscriptionController.js
+++ controllers/workshops/inscriptionController.js
@@ -5,16 +5,17 @@ function addInscription(req, result) {
     let idUser = req.body.idUser;
     let idWorkshop = req.body.idWorkshop;
     inscriptionFunctions.addInscription(idUser, idWorkshop, (error, success) => {
-        if (error) {
-            throw error;
-            return;
+        if (!error) {
+            result.json(success)
+        } else {
+            result.status(400).send(error)
         }
-        result.json(success)
     })
 }
-function getInscription(req, result){
-    inscriptionFunctions.getInscription((error, success)=>{
-        if(error){
+
+function getInscription(req, result) {
+    inscriptionFunctions.getInscription((error, success) => {
+        if (error) {
             throw error;
             return;
         }
--- controllers/workshops/inscriptionFunctions.js
+++ controllers/workshops/inscriptionFunctions.js
@@ -3,28 +3,35 @@ const connection = require("../../database/db-config")
 function addInscription(idUser, idWorkshop, callback) {
     const sql = `SELECT filled from workshop WHERE workshop_id = ?`;
     connection.query(sql, [idWorkshop], function (error, rows, fields) {
-        console.log(error)
         if (!error) {
-            console.log(rows)
             let newFilled = rows[0].filled + 1
             const sql2 = `UPDATE workshop SET filled = ? where workshop_id = ?`
             connection.query(sql2, [newFilled, idWorkshop], function (error, results, fields) {
                 if (!error) {
                     const sql2 = `INSERT INTO inscription (workshop_id, user_id) VALUES(?,?)`
                     connection.query(sql2, [idWorkshop, idUser], function (error, results, fields) {
-                        if (error) callback(error);
-                        callback(null, {
-                            success: true,
-                            message: "Inscrito no Workshop!"
-                        })
+                        if (error) {
+                            callback({
+                                message: "Já inscrito!"
+                            })
+                        } else {
+                            callback(null, {
+                                success: true,
+                                message: "Inscrito no Workshop!"
+                            })
+                        }
                     });
                 }
             });
+        } else {
+            callback({
+                message: "Workshop sem vagas!"
+            })
         }
     });
 }
 
-function getInscription(callback){
+function getInscription(callback) {
     let sql = `SELECT * from inscription`;
     connection.query(sql, function (error, rows, result) {
         if (error) callback(error);
